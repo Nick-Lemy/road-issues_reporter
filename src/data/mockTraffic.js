108fa68a633843a6dd709dca98d4666d08a0a0ba
Nick-Lemy <n.kayiranga@alustudent.com>
2025-10-17 00:45:41 +0200

    Remove mock issue and traffic data for cleaner codebase

--- src/data/mockTraffic.js
+++ src/data/mockTraffic.js
@@ -2,36 +2,7 @@
 // Each segment has an id, name, coordinates (array of lat,lng pairs), and congestion state
 
 export const initialTrafficSegments = [
-    {
-        id: 'kk_1',
-        name: 'KN 1 Ave (Example)',
-        coords: [
-            [-1.940, 30.058],
-            [-1.941, 30.060],
-            [-1.942, 30.062]
-        ],
-        congestion: 'green' // green, yellow, red
-    },
-    {
-        id: 'kk_2',
-        name: 'KN 2 Rd (Example)',
-        coords: [
-            [-1.946, 30.058],
-            [-1.944, 30.060],
-            [-1.943, 30.063]
-        ],
-        congestion: 'yellow'
-    },
-    {
-        id: 'kk_3',
-        name: 'KN 3 St (Example)',
-        coords: [
-            [-1.950, 30.055],
-            [-1.948, 30.058],
-            [-1.945, 30.060]
-        ],
-        congestion: 'red'
-    }
+
 ];
 
 // Simple simulator to randomly change congestion over time
